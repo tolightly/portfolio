29b9467ec52d888d148f2c1de1aeb266173a8d11
toLight <denytolighty@gmail.com>
2026-05-15 21:11:39 +0300

    smooth animations

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible'); // ← додай це
     }
   });
 }, { threshold: 0.1 });
